5fedb9972b5fd04818e1bc132c6ea5238d39ac7d
ssing119 <43161218+ssing119@users.noreply.github.com>
2019-07-14 19:35:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 //var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+var randomImageSource = "dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -14,7 +14,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
